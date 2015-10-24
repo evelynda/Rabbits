8d3f4360cf7cc7c99d999c3dbdba126757cbaf69
evelynda <evelynda99@gmail.com>
2015-10-24 13:41:06 +0000

    initial commit

---
+++ test.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'yaml/store'
+CHOICES = {
+  'happy'        => 'Happy',
+  'divaka'       => 'Дивака',
+  'krivoto'      => 'Кривото',
+  'ugo'          => 'Уго',
+  'mr-pizza'     => 'Мистър Пица',
+  'sun-moon'     => 'Слънце луна',
+  'soul-kitchen' => 'Soul Kitchen',
+}
+post '/cast' do
+  @title = 'Благодарим за вашия глас!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    if @store['votes'] == nil
+      @store['votes'] = {}
+    end
+
+    if @store['votes'][@vote]
+      @store['votes'][@vote] = @store['votes'][@vote] + 1
+    else
+      @store['votes'][@vote] = 1
+    end
+  end
+  erb :cast
+end
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
+# 5.times do puts "Yeah" end
+get '/' do
+  erb :evapp
+end
